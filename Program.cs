f53cc4d5dbe7d7d4bd01257b036445fca8f97aee
mhamad-jamal <77098020+mhamad-jamal@users.noreply.github.com>
2021-01-09 12:48:45 +0300

    firsts

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       Console.WriteLine("\nWhat is your born Year ? ");
+var input = Console.ReadLine();
+var born = Int32.Parse(input);
+var dateAndTime = DateTime.Now;
+var date = dateAndTime.Year;
+var age =  date - born;
+
+Console.WriteLine($"\nyou are  {age}  year old");
+Console.Write("\nPress any key to exit...");
+Console.ReadKey(true);
+        }
+    }
+}
